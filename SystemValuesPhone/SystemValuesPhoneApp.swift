303ff6c5cf043404df1659655fed67a719e836d8
AlgunRomper <lesonen.ekaterina@gmail.com>
2023-12-27 18:05:24 +0000

    Initial Commit

---
+++ SystemValuesPhone/SystemValuesPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemValuesPhoneApp.swift
+//  SystemValuesPhone
+//
+//  Created by Algun Romper  on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SystemValuesPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
