9caa939cb727e758d8c97b73d569f1c8555a0ce8
AlgunRomper <lesonen.ekaterina@gmail.com>
2023-12-27 19:02:52 +0000

    Added functions

---
+++ SystemValuesPhone/PhoneInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  PhoneInfoView.swift
+//  SystemValuesPhone
+//
+//  Created by Algun Romper  on 27.12.2023.
+//
+
+import SwiftUI
+
+struct PhoneInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhoneInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhoneInfoView()
+    }
+}
---
+++ SystemValuesPhone/ViewModelSystemValues.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelSystemValues.swift
+//  SystemValuesPhone
+//
+//  Created by Algun Romper  on 27.12.2023.
+//
+
+import Foundation
